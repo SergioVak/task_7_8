0b1bb70972bea58b15b4026541f6df3d02e4a108
SergioVak <45003961+SergioVak@users.noreply.github.com>
2019-12-23 14:49:02 +0200

    commit

--- task_7_8_Sequences/Sequences.Tests/GetSequenceTests.cs
+++ task_7_8_Sequences/Sequences.Tests/GetSequenceTests.cs
@@ -10,7 +10,7 @@ namespace Sequences.Tests
         [InlineData(20, "1 2 3 4 ")]
         [InlineData(100, "1 2 3 4 5 6 7 8 9 ")]
         [InlineData(100, "1 2 3 4 5 6 7 8 9 10 ")]
-        public void NotEmptySequnceTest(int border, string expected)
+        public void ReturnSequnceTest(int border, string expected)
         {
             string actual = string.Empty;
 
